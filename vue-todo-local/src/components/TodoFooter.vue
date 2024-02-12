703b8465722eeb7f11555107b4aab32f949cfb19
goandstop42 <goandstop42@naver.com>
2024-02-12 12:04:15 +0900

    init

---
+++ vue-todo-local/src/components/TodoFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="clearAllConteainer">
+    <span class="clearAllbtn" v-on:click="clearTodo">Clear All</span>
+  </div>
+</template>
+<script>
+
+export default {
+  methods: {
+    clearTodo: function () {
+      localStorage.clear();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.clearAllConteainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllbtn {
+  color: #e20303;
+  display: block;
+}
+
+</style>
---
+++ vue-todo-local/src/components/TodoList.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" >
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem,index)"></i>
+        <span v-bind:class="{textCompleted:todoItem.completed}">{{todoItem.item}}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+         <i class="fa-solid fa-trash"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+
+    export default {
+      data: function () {
+        return {
+          todoItems:[]
+        }
+      },
+      methods: {
+        removeTodo: function (todoItem, index) {
+          localStorage.removeItem(todoItem);
+          this.todoItems.splice(index, 1)
+        },
+        toggleComplete: function (todoItem, index) {
+          todoItem.completed = !todoItem.completed;
+          console.log(todoItem.item);
+          // 토글 상태를 localstorage에 반영
+          localStorage.removeItem(todoItem.item);
+          localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+          console.log(index);
+
+        }
+      },
+      created: function () {
+        if(localStorage.length > 0){
+          for(var i = 0; i < localStorage.length ; i++){
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+/* transition css */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
